d51a6df553595c75d1b8c712720e0e14f7ba7410
ani-wsl <25386188+adahad@users.noreply.github.com>
2022-10-21 17:27:54 -0400

    Improve text styling of navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,21 +1,29 @@
 import { Link } from "react-router-dom";
-import { Navbar as Sidebar } from "@mantine/core";
+import { Anchor, Navbar as Sidebar } from "@mantine/core";
 import useStyles from "./Navbar.styles";
 
 function Navbar() {
   const { classes } = useStyles();
   return (
     <Sidebar width={{ sm: 200, lg: 300, base: 100 }} p="xs">
-      <Sidebar.Section>
+      <Sidebar.Section grow>
         <div>
-          <Link to="/">Home</Link>
+          <Anchor component={Link} to="/">
+            Home
+          </Anchor>
         </div>
+
         <div>
-          <Link to="new">Write</Link>
+          <Anchor component={Link} to="/create">
+            Create
+          </Anchor>
         </div>
       </Sidebar.Section>
+
       <Sidebar.Section className={classes.footer}>
-        <Link to="login">Login</Link>
+        <Anchor component={Link} to="/login">
+          Login
+        </Anchor>
       </Sidebar.Section>
     </Sidebar>
   );
