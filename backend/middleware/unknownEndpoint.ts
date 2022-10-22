2af4de6800a5c733b878dcb60b6f4446fa859b61
ani-wsl <25386188+adahad@users.noreply.github.com>
2022-10-22 19:51:21 -0400

    Successful backend reset

--- backend/middleware/unknownEndpoint.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-const unknownEndpoint = (request: Request, response: Response) => {
-  response.status(404).send({ error: "Unknown endpoint" });
-};
-
-export default unknownEndpoint;
