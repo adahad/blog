ecd29f1ff4ad307d01ef7cd8798ce63f29bbaa8d
ani-wsl <25386188+adahad@users.noreply.github.com>
2022-10-22 19:56:12 -0400

    Rename index to server, fix lint problems

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,14 @@
-import express, { Express, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const app: Express = express();
+const app = express();
 const port = process.env.PORT;
+
+if (!port) {
+  throw new Error("No port provided");
+}
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
