521b788c882a76ac271364236f1d6ca5bcca4718
ani-wsl <25386188+adahad@users.noreply.github.com>
2022-10-23 03:45:51 -0400

    toJSON for posts doesn't return id

--- backend/models/post.ts
+++ backend/models/post.ts
@@ -2,10 +2,25 @@
 import { Schema, model } from "mongoose";
 import { Post } from "../types";
 
-const postSchema = new Schema<Post>({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-});
+const postSchema = new Schema<Post>(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars, object-shorthand, func-names
+      transform: function (_document, returnedObject) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        returnedObject.id = returnedObject._id.toString();
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        delete returnedObject._id;
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        delete returnedObject.__v;
+      },
+    },
+  }
+);
 
 const PostModel = model<Post>("Post", postSchema);
 
