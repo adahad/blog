ab90a812b862684aaa1ee6c9467d9eec7c4848a6
ani-wsl <25386188+adahad@users.noreply.github.com>
2022-10-22 20:29:15 -0400

    Fixed compile issue
    Problem was that nanoid dropped support for commonJS in version 4.
    Downgraded it to version 3.34

--- backend/server.ts
+++ backend/server.ts
@@ -1,19 +1,34 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import postsRouter from "./controllers/posts";
 
 dotenv.config();
 
-const app = express();
 const port = process.env.PORT;
-
 if (!port) {
   throw new Error("No port provided");
 }
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+const app = express();
+app.use(express.json());
+
+app.use("/", postsRouter);
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
+  console.log(`Server running on ${port}`);
 });
+
+// const app = express();
+// const port = process.env.PORT;
+
+// if (!port) {
+//   throw new Error("No port provided");
+// }
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Express + TypeScript Server");
+// });
+
+// app.listen(port, () => {
+//   console.log(`[server]: Server is running at https://localhost:${port}`);
+// });
