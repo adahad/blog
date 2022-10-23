5f19509d672ee04868f5800a6bc54e8e6c74c8a2
ani-wsl <25386188+adahad@users.noreply.github.com>
2022-10-23 01:49:34 -0400

    Implement MongoDB connection

--- backend/server.ts
+++ backend/server.ts
@@ -1,19 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import postsRouter from "./controllers/posts";
 
 dotenv.config();
 
-const port = process.env.PORT;
-if (!port) {
+const { PORT, MONGODB_URI } = process.env;
+if (!PORT) {
   throw new Error("No port provided");
 }
 
+if (!MONGODB_URI) {
+  throw new Error("No MONGODB_URI provided");
+}
+
 const app = express();
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error: Error) =>
+    console.log("Error connecting to MongoDB", error.message)
+  );
+
 app.use(express.json());
 
 app.use("/", postsRouter);
 
-app.listen(port, () => {
-  console.log(`Server running on ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
