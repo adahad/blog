33b34dfd54f2f2214f1e9bbf15340c315f1c077f
ani-wsl <25386188+adahad@users.noreply.github.com>
2022-10-22 20:30:31 -0400

    Cleaned up comments and unused imports

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import postsRouter from "./controllers/posts";
 
@@ -17,18 +17,3 @@ app.use("/", postsRouter);
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
 });
-
-// const app = express();
-// const port = process.env.PORT;
-
-// if (!port) {
-//   throw new Error("No port provided");
-// }
-
-// app.get("/", (req: Request, res: Response) => {
-//   res.send("Express + TypeScript Server");
-// });
-
-// app.listen(port, () => {
-//   console.log(`[server]: Server is running at https://localhost:${port}`);
-// });
