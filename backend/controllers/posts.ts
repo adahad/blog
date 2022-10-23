91b2e542d0b31a479fca34c4c740e76167f6d8d9
ani-wsl <25386188+adahad@users.noreply.github.com>
2022-10-23 03:14:32 -0400

    Add controllers for posts to MongoDB

--- backend/controllers/posts.ts
+++ backend/controllers/posts.ts
@@ -1,46 +1,67 @@
-import express, { Request, Response } from "express";
-import * as testData from "../tests/testData.json";
-import { Post, isPost } from "../types";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { NextFunction, Request, Response } from "express";
+import asyncHandler from "express-async-handler";
+import { isPost } from "../types";
+import Post from "../models/post";
 
 const router = express.Router();
 
-let posts: Post[] = testData.samplePosts;
+router.get(
+  "/",
+  asyncHandler(
+    async (request: Request, response: Response, next: NextFunction) => {
+      const posts = await Post.find({});
+      response.json(posts);
+    }
+  )
+);
 
-router.get("/", (request: Request, response: Response) => {
-  response.json(posts);
-});
+router.get(
+  "/posts/:id",
+  asyncHandler(
+    async (request: Request, response: Response, next: NextFunction) => {
+      const id = request.params.id;
+      const post = await Post.findById(id);
+      response.json(post);
+    }
+  )
+);
 
-// router.get("/posts/:id", (request: Request, response: Response) => {
-//   const id = request.params.id;
-//   const queriedPost = posts.find((post) => post.id === id);
-//   if (queriedPost) {
-//     response.json(queriedPost);
-//   } else {
-//     response.status(404).end();
-//   }
-// });
+router.delete(
+  "/posts/:id",
+  asyncHandler(
+    async (request: Request, response: Response, next: NextFunction) => {
+      const id = request.params.id;
 
-// router.delete("/posts/:id", (request: Request, response: Response) => {
-//   const id = request.params.id;
-//   posts = posts.filter((post) => post.id !== id);
-//   response.status(204).end();
-// });
+      await Post.findByIdAndDelete(id);
+      response.status(204).end();
+    }
+  )
+);
 
-router.post("/posts", (request: Request, response: Response) => {
-  if (!request.body) {
-    return response.status(400).json({ error: "No request body" });
-  }
-  if (!isPost(request.body)) {
-    return response.status(400).json({ error: "Header or title missing" });
-  }
+router.post(
+  "/posts",
+  asyncHandler(
+    async (request: Request, response: Response, next: NextFunction) => {
+      if (!request.body) {
+        response.status(400).json({ error: "No request body" });
+        return;
+      }
+      if (!isPost(request.body)) {
+        response.status(400).json({ error: "Header or title missing" });
+        return;
+      }
 
-  const newPost: Post = {
-    title: request.body.title,
-    content: request.body.content,
-  };
+      const newPost = new Post({
+        title: request.body.title,
+        content: request.body.content,
+      });
 
-  posts = posts.concat(newPost);
-  return response.json(posts);
-});
+      await newPost.save();
+
+      response.json(newPost);
+    }
+  )
+);
 
 export default router;
