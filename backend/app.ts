86f922865e4c3c99bd009d2cb088bab963e321cc
ani-wsl <25386188+adahad@users.noreply.github.com>
2022-10-24 02:13:37 -0400

    Refactor server into server.ts and app.ts

---
+++ backend/app.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import postsRouter from "./controllers/posts";
+import loginRouter from "./controllers/login";
+import signupRouter from "./controllers/signup";
+import unknownEndpoint from "./middleware/unknownEndpoint";
+
+dotenv.config();
+
+const NODE_ENV = process.env.NODE_ENV;
+
+const MONGODB_URI =
+  NODE_ENV === "TEST" ? process.env.TEST_URI : process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Database connection URI not provided");
+}
+
+const app = express();
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error: Error) =>
+    console.log("Error connecting to MongoDB", error.message)
+  );
+
+app.use(express.json());
+
+app.use("/", postsRouter);
+app.use("/login", loginRouter);
+app.use("/signup", signupRouter);
+
+app.use(unknownEndpoint);
+
+export default app;
--- backend/server.ts
+++ backend/server.ts
@@ -1,38 +1,12 @@
-import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
-import postsRouter from "./controllers/posts";
-import loginRouter from "./controllers/login";
-import signupRouter from "./controllers/signup";
-import unknownEndpoint from "./middleware/unknownEndpoint";
+import app from "./app";
 
 dotenv.config();
 
-const { PORT, MONGODB_URI } = process.env;
+const { PORT } = process.env;
 if (!PORT) {
   throw new Error("No port provided");
 }
-
-if (!MONGODB_URI) {
-  throw new Error("No MONGODB_URI provided");
-}
-
-const app = express();
-
-mongoose
-  .connect(MONGODB_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error: Error) =>
-    console.log("Error connecting to MongoDB", error.message)
-  );
-
-app.use(express.json());
-
-app.use("/", postsRouter);
-app.use("/login", loginRouter);
-app.use("/signup", signupRouter);
-
-app.use(unknownEndpoint);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
