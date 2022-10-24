e474a4b95a7ccea13682caf6feb038062255ac78
ani-wsl <25386188+adahad@users.noreply.github.com>
2022-10-24 15:48:05 -0400

    Rewrote test to get all posts, added helper funcs
    Get all test will now check length, and test against all posts in
    testPosts
    Added isPostArray
    Changed tests from func declarations to arrow funcs

--- backend/tests/postsController.test.ts
+++ backend/tests/postsController.test.ts
@@ -1,25 +1,33 @@
 import mongoose from "mongoose";
 import supertest from "supertest";
 import app from "../app";
-import Post from "../models/post";
+import PostModel from "../models/post";
 import * as helper from "./testHelper";
+import { isPostArray, Post } from "../types";
 
 const api = supertest(app);
 
 const testPosts = helper.initialPosts;
 
 beforeEach(async () => {
-  await Post.deleteMany({});
+  await PostModel.deleteMany({});
 
-  const postObjects = testPosts.map((post) => new Post(post));
+  const postObjects = testPosts.map((post) => new PostModel(post));
   const postPromises = postObjects.map((post) => post.save());
   await Promise.all(postPromises);
 });
 
-describe("GET: /posts", () => {
-  test("Correct number of notes is returned", async () => {
+describe("GET: /", () => {
+  test("Get all posts correctly", async () => {
     const response = await api.get("/");
-    expect(response.body).toHaveLength(3);
+    expect(response.body).toHaveLength(testPosts.length);
+    expect(isPostArray(response.body)).toBe(true);
+    const body: Post[] = isPostArray(response.body) ? response.body : [];
+
+    for (let i = 0; i < testPosts.length; i++) {
+      expect(body[i].title).toBe(testPosts[i].title);
+      expect(body[i].content).toBe(testPosts[i].content);
+    }
   });
 });
 
--- backend/types.ts
+++ backend/types.ts
@@ -21,26 +21,48 @@ interface UserSignup extends UserBase {
   name: string;
 }
 
-function isPost(unknown: unknown): unknown is Post {
+const isPost = (unknown: unknown): unknown is Post => {
   return (
     (unknown as Post).title !== undefined &&
     (unknown as Post).content !== undefined
   );
-}
+};
 
-function isUserSignup(unknown: unknown): unknown is UserSignup {
+const isUserSignup = (unknown: unknown): unknown is UserSignup => {
   return (
     (unknown as UserSignup).username !== undefined &&
     (unknown as UserSignup).password !== undefined &&
     (unknown as UserSignup).name !== undefined
   );
-}
+};
 
-function isUserBase(unknown: unknown): unknown is UserBase {
+const isUserBase = (unknown: unknown): unknown is UserBase => {
   return (
     (unknown as UserBase).username !== undefined &&
     (unknown as UserBase).password !== undefined
   );
-}
+};
 
-export { Post, isPost, User, UserBase, isUserBase, UserSignup, isUserSignup };
+const isPostArray = (unknown: unknown): unknown is Post[] => {
+  if (!Array.isArray(unknown)) {
+    return false;
+  }
+  // for (let i = 0; i < unknown.length; i++) {
+  //   if (!isPost(unknown)) {
+  //     return false;
+  //   }
+  // }
+  // return true;
+  return unknown.every(isPost);
+};
+
+export {
+  Post,
+  isPost,
+  User,
+  UserBase,
+  isUserBase,
+  UserSignup,
+  isUserSignup,
+  isPostArray,
+};
