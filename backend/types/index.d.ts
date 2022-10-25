47b4ccf3b80f5f9a15ffa5de47854cf386c7023b
ani-wsl <25386188+adahad@users.noreply.github.com>
2022-10-25 15:50:27 -0400

    Request extends hydrateddoc user

--- backend/types/index.d.ts
+++ backend/types/index.d.ts
@@ -1,9 +1,11 @@
-namespace Express {
-  interface Request {
-    token?: string;
-    user?: Document<unknown, any, User> &
-      User & {
-        _id: Types.ObjectId;
-      };
+import { HydratedDocument } from "mongoose";
+import { User } from "../types";
+
+declare global {
+  namespace Express {
+    interface Request {
+      token?: string;
+      user?: HydratedDocument<User> | null;
+    }
   }
 }
