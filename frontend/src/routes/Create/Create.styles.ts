187cb22a92a05582a501ce95fe09c2c0a4bb4cf7
ani-wsl <25386188+adahad@users.noreply.github.com>
2022-10-27 18:04:01 -0400

    Fix editor overflow issue

--- frontend/src/routes/Create/Create.styles.ts
+++ frontend/src/routes/Create/Create.styles.ts
@@ -3,17 +3,14 @@ import { createStyles } from "@mantine/core";
 const useStyles = createStyles((theme) => ({
   body: {
     height: "100%",
-    maxHeight: "100%",
-    padding: `5% 25%`,
+    // maxHeight: "100%",
+    padding: `5% 25% 0`,
     backgroundColor: `${theme.colors.gray[2]}`,
   },
-  stack: {
-    height: "100%",
-    maxHeight: "100%",
-  },
+  stack: {},
   editor: {
-    height: `85%`,
-    maxHeight: "85%",
+    height: `80vh`,
+    maxHeight: "80vh",
     overflow: "auto",
   },
 }));
