c7e1a27e4b5b5368b5ce633d96a6ff82e3eb90d1
ani-wsl <25386188+adahad@users.noreply.github.com>
2022-10-30 16:47:21 -0400

    Change post image sizing, title weight

---
+++ frontend/src/components/Posts/Post/Post.styles.ts
@@ -0,0 +1,9 @@
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  title: {
+    marginTop: theme.spacing.md,
+  },
+}));
+
+export default useStyles;
--- frontend/src/components/Posts/Post/Post.tsx
+++ frontend/src/components/Posts/Post/Post.tsx
@@ -1,6 +1,7 @@
 import { Card, Text, Image } from "@mantine/core";
 import { Link } from "react-router-dom";
 import { Post as PostType } from "../../../types";
+import useStyles from "./Post.styles";
 
 interface PostProps {
   post: PostType;
@@ -10,12 +11,16 @@ function Post({ post }: PostProps) {
   const { title, image, id } = post;
   const link = `/posts/${id}`;
 
+  const { classes } = useStyles();
+
   return (
     <Card shadow="sm" withBorder component={Link} to={link}>
       <Card.Section>
-        <Image src={image} />
+        <Image src={image} height={200} />
       </Card.Section>
-      <Text lineClamp={1}>{title}</Text>
+      <Text lineClamp={1} className={classes.title} weight={500}>
+        {title}
+      </Text>
     </Card>
   );
 }
